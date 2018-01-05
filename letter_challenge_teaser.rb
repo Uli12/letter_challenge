45f31ae523200e63e4571e358d85243876342311
Yurie Lorange <yl-mbp@Yuries-MacBook-Pro.local>
2018-01-05 11:48:16 -0700

    initial commit.

---
+++ letter_challenge_teaser.rb
@@ -0,0 +1,18 @@
+puts "input a group of letters"
+
+@char = gets.chomp.split('')
+
+def chr_to_num
+  @char.each{|c| puts @c = c.ord }
+end
+
+def collection
+  options = { @char => chr_to_num }
+  array = []
+  array.push(options)
+  @collection = array
+end
+
+puts collection
+
+
